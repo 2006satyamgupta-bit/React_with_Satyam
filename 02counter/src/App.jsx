5b37479da6204c0ee5b434a77c6b246b48f58905
satvikraina08 <satvikraina08@gmail.com>
2026-07-07 09:53:47 +0530

    counter file

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { use } from 'react'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+// not necessary to be named the function as setCounter
+  // let counter = 15;
+
+  const addValue = () => {
+    console.log("clicked", counter)
+    // counter = counter + 1
+    setCounter(counter + 1) 
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+
+
+    <button
+    onClick={addValue}>
+      Add value: {counter}</button>
+    <br />
+    <button
+    onClick={removeValue}>Remove value {counter}</button>
+    <p>footer: {counter} </p>
+    </>
+  )
+}
+
+export default App
